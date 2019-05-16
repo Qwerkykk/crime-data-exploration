4f1ed1e821ea962b3aeb60642a4000f3e18fe66e
billsioros <billsioros97@gmail.com>
2019-05-16 13:47:11 +0300

    Preprocessing is done i presume

--- src/reader.py
+++ src/reader.py
@@ -3,33 +3,33 @@ import pandas as pd
 
 class Reader:
 
-    headers = [
-        'INCIDENT_NUMBER',
-        'OFFENSE_CODE_GROUP',
-        'DISTRICT',
-        'SHOOTING',
-        'YEAR',
-        'MONTH',
-        'DAY_OF_WEEK',
-        'HOUR',
-        'Lat',
-        'Long'
-    ]
+    headers = {
+        'INCIDENT_NUMBER': str,
+        'OFFENSE_CODE_GROUP': str,
+        'DISTRICT': str,
+        'SHOOTING': str,
+        'YEAR': int,
+        'MONTH': int,
+        'DAY_OF_WEEK': str,
+        'HOUR': int,
+        'Lat': float,
+        'Long': float
+    }
 
     def __init__(self, filename):
 
         if not isinstance(filename, str):
             raise ValueError("'filename' is not an instance of 'str'")
 
-        print('<LOG>: Loading headers', self.headers)
+        print('<LOG>: Loading headers', list(self.headers.keys()))
 
-        self.data = pd.read_csv(filename, skipinitialspace=True, usecols=self.headers)
+        self.data = pd.read_csv(filename, dtype=self.headers, skipinitialspace=True, usecols=self.headers.keys())
 
         print('<LOG>:', len(self.data.index), 'rows [Before dropping NaN values]')
 
         self.data['SHOOTING'].fillna('N', inplace=True)
 
-        self.data.dropna()
+        self.data.dropna(inplace=True)
 
         print('<LOG>:', len(self.data.index), 'rows [After dropping NaN values]')
 
@@ -49,7 +49,7 @@ class Reader:
 
 if __name__ == "__main__":
 
-    reader = Reader('../data/head.csv')
+    reader = Reader('../data/crime.csv')
 
     print(reader.groupby('SHOOTING'))
 
