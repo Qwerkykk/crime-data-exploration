00e8cba7a4d0ca87f3af534cc4f3f4cac04dcd65
billsioros <billsioros97@gmail.com>
2019-05-16 17:11:51 +0300

    Year, Month, Day, District countplotting

--- src/plot.py
+++ src/plot.py
@@ -1,3 +1,59 @@
 
-import matplotlib
+import re
+
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+from reader import Reader
+
+class Plotter:
+
+    def __init__(self, reader):
+
+        if not isinstance(reader, Reader):
+            raise ValueError("'reader' is not an instrance of 'Reader'")
+
+        sb.set(style='whitegrid')
+
+        self.data = reader.data
+
+
+    def countplot(self, header, palette='Set3'):
+
+        if not isinstance(header, str):
+            raise ValueError("'header' is not an instrance of 'str'")
+
+        if header == 'DAY_OF_WEEK':
+            order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        else:
+            order = sorted(self.data[header].unique())
+
+        axes = sb.countplot(x=header, data=self.data, order=order, palette=palette)
+
+        axes.set(xlabel=header.replace('_', ' ').title(), ylabel='')
+
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    reader = Reader('../data/crime.csv')
+
+    plotter = Plotter(reader)
+
+    plotter.countplot('YEAR')
+
+    plotter.countplot('MONTH')
+
+    plotter.countplot('DAY_OF_WEEK')
+
+    plotter.countplot('DISTRICT')
+
+    # ax = sb.countplot(x='HOUR', data=reader.data)
+
+    # plt.show()
+
+    # ax = sb.countplot(x='YEAR', hue='SHOOTING', data=reader.data[reader.data['SHOOTING'] == 'Y'])
+
+    # plt.show()
 
